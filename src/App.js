93588f4a9f9d764adb75e5a34505de305a0c4a47
Nandagopal Kamaraj <nandgopal@rasheedfoundation.org>
2024-05-09 17:26:44 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App(){
+  return(
+    <div>
+      <h1>Welcome to My React App</h1>
+      <Hello />
+    </div>
+  );
+}
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello(){
+    return(
+        <div>
+            <p>Hello, World</p>
+        </div>
+    );
+}
+
+export default Hello;
